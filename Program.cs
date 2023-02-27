a30d47c67562e12d990f35609f7a078c6bde235b
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-27 19:37:42 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace BinarySearchTreeProblems
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Tree Problems");
+
+        }
+    }
+}
